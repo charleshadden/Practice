578d468b9956b29691b82e5fba97b0012bbe4580
Hadden <charles.hadden@accenturefederal.com>
2019-04-17 14:07:00 -0500

    intitial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		ArrayList<Soda> sodas = new ArrayList<>();
+
+		Soda soda1 = new Soda();
+		Soda soda2 = new Soda();
+		Soda soda3 = new Soda();
+
+		soda1.setName("BigRed");
+		if(soda1.getName() != "BigRed") {
+			System.out.println("fail");
+		} 
+		soda1.setPrice(1);
+		soda1.setHealthRating(2);
+		soda1.setType("soda");
+
+		soda2.setName("Mountain Dew");
+		soda2.setPrice(2);
+
+		soda3.setName("Coke");
+		soda3.setPrice(3);
+
+		sodas.add(soda1);
+		sodas.add(soda2);
+		sodas.add(soda3);
+
+		for (Soda soda : sodas) {
+			System.out.println(soda.getName());
+			System.out.println(soda.getHealthRating());
+			System.out.println(soda.getType());
+		}
+		soda1.drink();
+	}
+}
---
+++ src/Soda.java
@@ -0,0 +1,33 @@
+public class Soda extends Beverage {
+
+	private String name;
+	private int price;
+	
+
+	public Soda() {
+
+	}
+
+	public Soda(String name, int price) {
+		this.name = name;
+		this.price = price;
+
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
